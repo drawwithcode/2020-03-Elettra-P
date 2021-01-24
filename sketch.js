4330dd8c071063f133293ee5c504b8fc1323d40e
Elettra-P <71377175+Elettra-P@users.noreply.github.com>
2021-01-24 23:06:09 +0100

    new assignment

--- sketch.js
+++ sketch.js
@@ -1,104 +1,78 @@
-let run;
-let runFlip;
-let mySong;
-let songRate = 1;
-let myBubbles = [];
+let trianglesArray1 = [];
 
+
+let speedX;
+let speedY;
+let img;
+let song;
+let amp
 
 function preload() {
-  run = loadImage("./assets/images/running1.png");
-  runFlip = loadImage("./assets/images/running2.png");
-  mySong = loadSound("./assets/audio/Forget Me Not - Patrick Patrikios.mp3");
+  img = loadImage('assets/annie_lennox.jpg');
+  song = loadSound('assets/sweet_dreams.mp3');
+
 }
 
 function setup() {
-  mySong.loop();
-  //randomization of bubble parameters
-  for (let i = 0; i < 1000; i++) {
-    const options = {
-      x: width / 2 + random(-10, 10),
-      y: height / 2 + random(-10, 0),
-      radius: random(70, 130)
-    }
-        myBubbles.push(new Bubble(options));
-  }
+  createCanvas(windowWidth, windowHeight);
+  song.loop();
+  amp = new p5.Amplitude();
+  amp.setInput(song);
+
 
 }
-
 
 
 function draw() {
-    if (!mySong.isPlaying()) {
-    mySong.play()
+  image(img, 0, 0, width, width * img.height / img.width);
+
+  let speed = amp.getLevel();
+
+  let player = song.isPlaying();
+  if (!player) {
+    song.play();
   }
 
+  for (let i = 0; i < 100; i++) {
+    trianglesArray1[i] = new Triangles(random(0, width), random(0, height), 20 * random(-1, 1) * speed, 20 * random(-1, 1) * speed)
 
-  createCanvas(windowWidth, windowHeight);
-  background("#EC9A29")
-
-
-  for (let i = 0; i < myBubbles.length; i++) {
-    myBubbles[i].run();
+    for (let i = 0; i < trianglesArray1.length; i++) {
+      trianglesArray1[i].paintTriangle(255);
+      trianglesArray1[i].moveTriangle();
+    }
   }
 
-//when the key up arrow is pressed the image changesand the song rates gets faster
-  imageMode(CENTER);
-  if (keyIsDown(UP_ARROW)) {
-    songRate = songRate + 0.01;
-    image(runFlip, windowWidth / 2, 6 * windowHeight / 7, runFlip.width, runFlip.height);
-    mySong.rate(songRate)
-  } else {
-    image(run, windowWidth / 2, 6 * windowHeight / 7, run.width, run.height)
-  }
-
-//how to play text
-  push();
-  noStroke();
-  fill("#A8201A");
-  rect(10, 850, 410, 1000, 20);
-  pop();
-  push();
-  textFont("Courier New");
-  textStyle(BOLD)
-  textSize(24);
-  fill("#DAD2D8");
-  text("Press the ARROW UP key up to make the Joker run, and don't forget to turn the volume up!", 30, 900, 400, 400);
-  pop();
 }
 
 
-//class and methods to create bubbles randomly sized and randomly moving in the background
-class Bubble {
-  constructor({
-    x,
-    y,
-    radius
-  } = {}) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-
+class Triangles {
+  constructor(triX, triY, speedX, speedY) {
+    this.X = triX
+    this.Y = triY
+    this.speedX = speedX
+    this.speedY = speedY
   }
 
-  show() {
-    push();
-    stroke("#143642");
-    fill("#0F8B8D")
-    ellipse(this.x, this.y, this.radius * 2)
-    pop()
+  paintTriangle(red) {
+    noStroke()
+    triangle(this.X, this.Y, this.X + 25, this.Y + 40, this.X - 25, this.Y + 40)
   }
 
-  move() {
-    this.x += random(-windowWidth, windowWidth)
-    this.y += random(100, -100)
-  }
+  moveTriangle() {
+    this.X += this.speedX;
+    this.Y += this.speedY;
 
-  run() {
-    this.move();
-    this.show();
+    if (this.Y > height || this.Y < 0) {
+      this.speedY *= -1;
+      fill(random(255), random(255), random(255))
+    }
+
+    if (this.X > width || this.X < 0) {
+      this.speedX *= -1;
+      fill(random(255), random(255), random(255))
+    }
   }
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
